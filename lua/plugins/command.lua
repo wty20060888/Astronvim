50d7f5d220b815c9b45d65f001bd5a419d7a2348
王天宇 <wangtianyu@icloud.com>
2026-02-03 08:42:48 +0800

    Automatically switch to English when entering Normal mode

--- lua/plugins/command.lua
+++ lua/plugins/command.lua
@@ -1,3 +1,20 @@
 return {
-  -- TODO: 自定义cmdline
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      autocmds = {
+        -- first key is the augroup name
+        im_switch = {
+          -- the value is a list of autocommands to create
+          {
+            -- event is added here as a string or a list-like table of events
+            event = "InsertLeave",
+            command = "silent !im-select com.apple.keylayout.ABC",
+            desc = "Automatically switch to English when entering Normal mode",
+          },
+        },
+      },
+    },
+  },
 }
