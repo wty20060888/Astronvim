5399a8f0ebf3631c9a193083ea04b9bb2feb3fb6
王天宇 <wangtianyu@icloud.com>
2026-03-02 09:18:32 +0800

    禁用arduinols

--- lua/plugins/arduinolsp.lua
+++ lua/plugins/arduinolsp.lua
@@ -1,19 +1,16 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
--- FIXME:arduino_language_server 问题
+-- 删除上面这行注释以激活此文件
 return {
   "AstroNvim/astrolsp",
-  -- we need to use the function notation to get access to the `lspconfig` module
-  ---@param opts AstroLSPOpts
   opts = function(plugin, opts)
-    -- insert "prolog_lsp" into our list of servers
+    -- 插入 "arduino_language_server" 到服务器列表
     opts.servers = opts.servers or {}
     table.insert(opts.servers, "arduino_language_server")
 
-    -- extend our configuration table to have our new prolog server
+    -- 扩展配置表以包含新的 arduino 服务器
     opts.config = require("astrocore").extend_tbl(opts.config or {}, {
-      -- this must be a function to get access to the `lspconfig` module
       arduino_language_server = {
-        -- the command for starting the server
+        -- 启动服务器的命令
         cmd = {
           "arduino-language-server",
           "-cli-config",
@@ -25,8 +22,41 @@ return {
           "-clangd",
           "clangd",
         },
-        -- the filetypes to attach the server to
-        filetypes = { "arduino" },
+        -- 服务器支持的文件类型
+        filetypes = { "arduino", "ino" },
+        root_dir = require("lspconfig.util").root_pattern("sketch.yaml", ".git"),
+
+        -- 修复：使用安全的方式获取 capabilities
+        capabilities = (function()
+          -- 尝试从 astrolsp 获取 capabilities
+          local ok, astrolsp_cap = pcall(require, "astrolsp")
+          if ok and astrolsp_cap.capabilities then
+            local cap = vim.deepcopy(astrolsp_cap.capabilities)
+            cap.semanticTokensProvider = nil
+            return cap
+          end
+
+          -- 如果 astrolsp 不可用，则使用默认的 LSP capabilities
+          local cap = vim.lsp.protocol.make_client_capabilities()
+          -- 添加常见的能力
+          cap.textDocument = {
+            completion = {
+              completionItem = {
+                snippetSupport = true,
+                commitCharactersSupport = true,
+                deprecatedSupport = true,
+                preselectSupport = true,
+                tagSupport = { valueSet = { 1 } },
+                insertReplaceSupport = true,
+                resolveSupport = {
+                  properties = { "documentation", "detail", "additionalTextEdits" },
+                },
+              },
+            },
+          }
+          cap.semanticTokensProvider = nil
+          return cap
+        end)(),
       },
     })
   end,
