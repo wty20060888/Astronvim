25814d95b1358ed4127e6d1c8cee48f5d3a73aa6
王天宇 <wangtianyu@icloud.com>
2026-03-17 11:41:13 +0800

    update arduino

--- lua/plugins/arduino.lua
+++ lua/plugins/arduino.lua
@@ -1,23 +1,62 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- TODO: 用户自定义命令
 return {
-  "yuukiflow/Arduino-Nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  ft = { "arduino" },
-  config = function()
-    -- Load Arduino plugin for .ino files
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "arduino",
-      callback = function() require "Arduino-Nvim" end,
-    })
-    -- keymap
-    -- require "Arduino-Nvim"
-    -- vim.keymap.del("n", "<Leader>au")
-    -- vim.keymap.set("n", "<Leader>ad", ":InoUpload<CR>", { silent = true }) -- Upload code
-    -- 这样修改配置
-    -- local arduino_nvim = require "Arduino-Nvim"
-    -- arduino_nvim.upload = function() vim.notify "hahaha" end
-  end,
+  vim.api.nvim_create_user_command(
+    "Pioinit",
+    function(opts) -- 注意这里的 opts 参数，用于接收命令后的输入
+      -- 1. 初始化 PlatformIO Uno 工程
+      vim.notify("🔧 初始化 Arduino Uno 工程...", vim.log.levels.INFO)
+      vim.fn.system "pio project init --board uno"
+
+      -- 2. 生成 compile_commands.json（供 clangd 代码补全）
+      vim.notify("🔧 生成编译数据库...", vim.log.levels.INFO)
+      vim.fn.system "pio run -t compiledb"
+
+      -- 3. 定义 main.cpp 示例代码内容
+      local main_code = table.concat({
+        "#include <Arduino.h>",
+        "void setup() {",
+        "    pinMode(LED_BUILTIN, OUTPUT);",
+        "    Serial.begin(9600);",
+        "}",
+        "void loop() {",
+        "    digitalWrite(LED_BUILTIN, HIGH);",
+        "    delay(1000);",
+        "    digitalWrite(LED_BUILTIN, LOW);",
+        "    delay(1000);",
+        "}",
+      }, "\n")
+
+      -- 4. 写入代码到 src/main.cpp
+      local file = io.open("src/main.cpp", "w")
+      if file then
+        file:write(main_code)
+        file:close()
+      end
+
+      -- 6. 完成提示
+      vim.notify("✅ Arduino Uno 工程初始化完成！", vim.log.levels.SUCCESS)
+    end,
+    {
+      desc = "Init Arduino Project",
+      nargs = "?", -- 表示命令接受0个或1个参数
+    }
+  ),
+  vim.api.nvim_create_user_command(
+    "Pioupload",
+    function(opts) -- 注意这里的 opts 参数，用于接收命令后的输入
+      -- 1. 编译 PlatformIO Uno 工程
+      vim.cmd "silent wall"
+      vim.cmd "!pio run -d ../"
+
+      -- 2. 上传PlatformIO Uno 工程
+      vim.cmd "!pio run --target upload -d ../"
+
+      -- 3. 完成提示
+      vim.notify("✅ Arduino Uno 工程上传完成！", vim.log.levels.SUCCESS)
+    end,
+    {
+      desc = "Upload Arduino Project",
+      nargs = "?", -- 表示命令接受0个或1个参数
+    }
+  ),
 }
