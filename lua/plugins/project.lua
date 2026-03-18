77982b103fda2ec8a380c5b1ed3bee42e6890d39
王天宇 <wangtianyu@icloud.com>
2026-03-18 13:49:37 +0800

    update project

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -5,7 +5,7 @@ return {
       -- 项目根标志
       detection_methods = { "pattern", "lsp" },
       -- 从当前文件目录向上递归查找，只要找到数组中任意一个标志，就会把该目录认定为项目根目录（数组靠前的标志优先级更高）。
-      patterns = { ".git", ".pio" },
+      patterns = { ".git", ".pio", ".venv" },
       -- 记住最近打开的项目
       show_hidden = false,
       silent_chdir = true, -- 静默切换工作目录
